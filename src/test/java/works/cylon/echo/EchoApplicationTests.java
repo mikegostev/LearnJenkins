de5e895ec71c1c9686282a08be13ee19d19b7509
mike <mike@latitude>
2022-05-11 12:43:55 +0100

    Initial commit

---
+++ src/test/java/works/cylon/echo/EchoApplicationTests.java
@@ -0,0 +1,13 @@
+package works.cylon.echo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
